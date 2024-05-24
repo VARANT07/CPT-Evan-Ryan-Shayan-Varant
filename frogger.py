7556db6e8bea93580c37dc084c0b84e2e4eb3bf0
RyanWang2007 <159087895+RyanWang2007@users.noreply.github.com>
2024-05-24 14:06:41 -0400

    Update frogger.py
    Made the startup for the game

--- frogger.py
+++ frogger.py
@@ -1,1 +1,24 @@
 import pygame
+#Game
+pygame.init()
+WIDTH = 1000
+HEIGHT = 750
+SIZE = (WIDTH, HEIGHT)
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+#Functions
+
+# Global Variables
+
+#Game running
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(30)
+    
+
+pygame.quit()
